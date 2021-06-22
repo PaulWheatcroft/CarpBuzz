3c8d211a88af5a3879a556b6d49cd47087ea4c5d
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-06-22 17:41:19 +0000

    Initial MongoDB setup

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def hello():
-    return "Hello Carp Buzzers!!"
+@app.route("/get_fisheries")
+def get_fisheries():
+    fisheries = mongo.db.fisheries.contact.find()
+    return render_template("fisheries.html", fisheries=fisheries)
 
 
 if __name__ == "__main__":
