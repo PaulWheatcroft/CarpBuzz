f3f2ba60cd2fd7f171a206d362884631b1479185
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-06-28 22:45:25 +0000

    Fontawesome added to mobile menu

--- static/js/script.js
+++ static/js/script.js
@@ -2,5 +2,7 @@ $(document).ready(function(){
     $(".dropdown-trigger").dropdown();
     $('.sidenav').sidenav({
       edge: 'right',
+      draggable: true,
+      closeOnClick: true
     });
   });
