321c819615f59f71a41d8016065400f76177bc53
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-07-04 13:28:26 +0000

    Add firshery form created

--- app.py
+++ app.py
@@ -104,6 +104,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_fishery", methods=["GET", "POST"])
+def add_fishery():
+    return render_template("add_fishery.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
