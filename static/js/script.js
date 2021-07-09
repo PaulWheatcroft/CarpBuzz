89beebead56b46cfcd44edcd8c4302b9d3aa5a5d
Paul Wheatcroft <paul_wheatcroft@hotmail.com>
2021-07-09 18:21:31 +0100

    Add materialize form elements to add review page

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $(".dropdown-trigger").dropdown();
     $('select').formSelect();
+    $('.datepicker').datepicker();
     $('.sidenav').sidenav({
       edge: 'right',
       draggable: true,
