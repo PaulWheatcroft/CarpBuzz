227eb04d1466be442df14cc047485683988d18dc
Paul Wheatcroft <paul_wheatcroft@hotmail.com>
2021-07-15 18:16:27 +0100

    Wiring up the fisheries filter

--- app.py
+++ app.py
@@ -32,6 +32,34 @@ def get_fisheries():
         tickets=tickets, payments=payments)
 
 
+@app.route("/filter_fisheries", methods=["GET", "POST"])
+def filter_fisheries():
+    # filter_list = []
+    print(request.form.get("wiltshire"))
+    # if bool(request.form.get("wiltshire"):
+    #     filter_list.append({"county": "wiltshire"})
+    # if bool(request.form.get("gloucestershire")):
+    #     filter_list.append({"county": "south gloucestershire"})
+    # if bool(request.form.get("north_somerset")):
+    #     filter_list.append({"county": "north somerset"})
+    # if bool(request.form.get("somerset")):
+    #     filter_list.append({"county": "somerset"})
+    # if bool(request.form.get("dorset")):
+    #     filter_list.append({"county": "dorset"})
+    # if bool(request.form.get("devon")):
+    #     filter_list.append({"county": "devon"})
+    # if bool(request.form.get("cornwall")):
+    #     filter_list.append({"county": "cornwall"})
+    # fisheries = mongo.db.fisheries.contact.find({"$and": filter_list})
+    # facilities = list(mongo.db.fisheries.facilities.find())
+    # tickets = list(mongo.db.fisheries.tickets.find())
+    # payments = list(mongo.db.fisheries.payment.find())
+    # return render_template(
+    #     "fisheries.html", fisheries=fisheries, facilities=facilities,
+    #     tickets=tickets, payments=payments)
+    return redirect(url_for("get_fisheries"))
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
